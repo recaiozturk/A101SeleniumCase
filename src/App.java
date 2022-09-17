fe4c184fee3212570376f46e1fac3cabeb8e7d48
Recai <recaiozturk54@gmail.com>
2022-09-17 13:24:16 +0300

    Last Touches

--- src/App.java
+++ src/App.java
@@ -40,127 +40,41 @@ public class App {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
-        WebDriver driver = new ChromeDriver();
-
-        //wait
-        //Declare and initialise a fluent wait
-        FluentWait wait = new FluentWait(driver);
-        
+        WebDriver driver = new ChromeDriver();     
         driver.manage().window().maximize();
-        // driver.manage().deleteAllCookies();
 
-        //corap ten rengi
-        // driver.get("https://www.a101.com.tr/giyim-aksesuar/penti-kadin-50-denye-pantolon-corabi-siyah/?integration_colour=S%C4%B0YAH#");
-        // System.out.println("giyim-aksesuar/penti-kadin-50-denye-pantolon-corabi-siyah SAYFASI ACILDI");
         controller.GetStart(driver);
 
-
-        //close cerez kullanım uyarısı
-        // driver.findElement(By.xpath("//*[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']")).click();
         controller.CloseCookieAlert(driver);
 
-
-        //siyah oldugunu dpğrula
-        // driver.findElement(By.xpath("/html/body/section/section[3]/div[2]/div[1]/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/a/img")).click();
-        // System.out.println("SİYAH DOĞRULANDI");
         controller.CheckBlack(driver);
 
-
-        //sepete ekle butonuna tıklanır
-        // driver.findElement(By.xpath("/html/body/section/section[3]/div[2]/div[1]/div/div[3]/div[2]/div[1]/button")).click();
-        // System.out.println("SEPETe EKLE BUTON TIKLANDI");
         controller.AddToBasket(driver);
+        WaitLittleBit(driver);
 
-       //Specify the timout of the wait      
-       //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
-       //System.out.println("BEkle");
-       Bekle(driver);
-       
-
-
-
-        //Sepet iGörüntüler Butonuna Tıklanır
-        // driver.findElement(By.xpath("//*[@id='js-modal-basket']/div[3]/a[3]")).click();  
-        // System.out.println("SEPETI GÖRÜNTÜLE TIKLANDI");
         controller.GoToBasket(driver);
-        Bekle(driver);
+        WaitLittleBit(driver);
 
-        //Sepeti Onayla Butonuna Tıklanır
-        // driver.findElement(By.xpath("/html/body/section/div[1]/div[2]/div/div[2]/div/a")).click();  
-        // System.out.println("SEPETI ONAYLA TIKLANDI");
         controller.AcceptToBasket(driver);
-        Bekle(driver);
+        WaitLittleBit(driver);
 
-        //Uye Olmadan devam et Butonuna tıklanır
-        // driver.findElement(By.xpath("/html/body/section/div[1]/div/div[1]/div[1]/div[2]/a")).click();  
-        // System.out.println("UYEOLMADAN DEVAM ET TIKLNADI");
         controller.ContuniewithoutSignIn(driver);
-        Bekle(driver);
+        WaitLittleBit(driver);
 
-        //eposta set et ve devam et e tıkla
-        // String  email="jsp10356@xcoxc.com";
-        // driver.findElement(By.xpath("/html/body/section/div[1]/div/div[2]/div/div/form/div[1]/input")).sendKeys(email);
-        // driver.findElement(By.xpath("/html/body/section/div[1]/div/div[2]/div/div/form/button")) .click();  // Devam Et Tıkla
         controller.SetToMail(driver);
-        Bekle(driver);
+        WaitLittleBit(driver);
 
-        //Adres olusturma
+        controller.SetAdress(driver);
+        controller.WaitWithThread(1000);
 
-        driver.findElement(By.xpath("/html/body/section/section/div/div[2]/div/div[1]/div/div[1]/div[2]/ul[2]/li/a")).click();  //Yeni adres olustur butonuna tıklıyoruz
-        //Adres Başlığı
-        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[2]/div/div/label/input")).sendKeys("Test Adres Başlık");
-        //İsim Giriyoruz
-        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[3]/div[1]/div/label/input")).sendKeys("Test İsim");
-        //Soyİsim Giriyoruz
-        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[3]/div[2]/div/label/input")).sendKeys("Test Soyİsim");
-        //Telefon Giriyoruz
-        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[4]/div/div/label/input")).sendKeys("111-111-1111");
-        //İl Giriyoruz
-        // driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[5]/div[1]/div/label/div/select")).sendKeys("İSTANBUL");
-        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[5]/div[1]/div/label/div/select/option[2]")).click();
-        Bekle(driver);
-        //İlçe Giriyoruz
-        //driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[5]/div[2]/div/label/div/select")).sendKeys("ADALAR");
-        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[5]/div[2]/div/label/div/select/option[2]")).click();
-        Bekle(driver);
-        //Mahalle Giriyoruz
-        //driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[6]/label/div/select")).sendKeys("NIZAM MAH");
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[6]/label/div/select/option[2]")).click();
-
-
-
-        //Burda sıkıntı war mahalleyi alamıyoruz BAKILACAKKKK:!!!!!!!!!!!!  thread ile cozduk devam eilecek
-        
-        // Select objSelect = new Select(driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[6]/label/div/select")));
-        // objSelect.getFirstSelectedOption();
-        
-        // objSelect.selectByValue("35388");
-        // objSelect.sendKeys("BURGAZADA MAH");
-        
-        Bekle(driver);
-        
-        
-        // //Adres Giriyoruz
-        // driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[7]/label/textarea")).sendKeys("Test Adresidir");
-        // //PostaKodu Giriyoruz
-        // driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[8]/div/div/label/input")).sendKeys("11111");
-
-        
-        
-
+        controller.ConfirmLastPage(driver);
         
     }
 
     //Bekleme Methodumuz
-    public static void Bekle(WebDriver driver){
-
-        
+    public static void WaitLittleBit(WebDriver driver){
+      
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
-        System.out.println("BEkle");
-
     }
 
-
-    
 }
--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,12 @@
+import java.time.Duration;
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Controller {
+
+    int randomNumber;
 
     //Siparişi istenilen ürünün sayfasını açar
     public void GetStart(WebDriver driver){
@@ -60,6 +65,89 @@ public class Controller {
         driver.findElement(By.xpath("/html/body/section/div[1]/div/div[2]/div/div/form/div[1]/input")).sendKeys(email);
         driver.findElement(By.xpath("/html/body/section/div[1]/div/div[2]/div/div/form/button")) .click(); 
         System.out.println("EPOSTA GİRİLDİ-DEVAM EDİLİYOR...");  
+    }
+
+
+    //Adres bilgilerini Giriyoruz
+    public void SetAdress(WebDriver driver){
+
+        driver.findElement(By.xpath("/html/body/section/section/div/div[2]/div/div[1]/div/div[1]/div[2]/ul[2]/li/a")).click();  //Yeni adres olustur butonuna tıklıyoruz
+        //Adres Başlığı
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[2]/div/div/label/input")).sendKeys("Test Adres Başlık");
+        //İsim Giriyoruz
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[3]/div[1]/div/label/input")).sendKeys("Test İsim");
+        //Soyİsim Giriyoruz
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[3]/div[2]/div/label/input")).sendKeys("Test Soyİsim");
+        //Telefon Giriyoruz
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[4]/div/div/label/input")).sendKeys("111-111-1111");
+        //İl Giriyoruz
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[5]/div[1]/div/label/div/select/option[2]")).click();       
+        //İlçe Giriyoruz
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[5]/div[2]/div/label/div/select/option[2]")).click();      
+        //Mahalle Giriyoruz       
+        WaitWithThread(1000);
+
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[6]/label/div/select/option[2]")).click();
+        //Açık Adres Giriyoruz
+        randomNumber = GenerateRandomNumber(1000);
+        
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[7]/label/textarea")).sendKeys("Test Adresidir"+randomNumber);
+        //posta kodu giriyoruz
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/div[8]/div/div/label/input")).sendKeys("11111");
+        WaitWithThread(1000);
+
+        //Keydet butonuna tıklıyoruz
+        driver.findElement(By.xpath("//*[@id='js-orders-modal-container']/div/div[2]/form/button[1]")).click();     
+        WaitWithThread(3000);
+
+        //RadioButtonTıklıyoruz
+        driver.findElement(By.xpath("/html/body/section/section/div/div[2]/div/div[1]/div/div[2]/form/div[2]/div[2]/ul/li/label/div[2]/div")).click(); 
+        WaitWithThread(3000);
+
+        //Keydet ve Devam et Butonuno Tıklıyoruz
+        driver.findElement(By.xpath("/html/body/section/section/div/div[2]/div/div[1]/div/div[2]/form/div[2]/button")).click();
+        WaitWithThread(1000);
+
+        System.out.println("ADRES GİRİLİP KAYDEDİLDİ-DEVAM EDİLİYOR");  
+
+
+    }
+
+    public void ConfirmLastPage(WebDriver driver){
+
+        String  confirmText =driver.findElement(By.xpath("/html/body/section/section/div/div[3]/div[2]/div[1]/div[2]/form[1]/div[1]/div[2]/div[5]/div/div[2]/b")).getText();
+
+        WaitWithThread(1000);
+
+        System.out.println( confirmText + "  yazısı teyit edildi");
+        System.out.println("TEST BİTTİ");
+    }
+
+    //ThreadSleep ile bekleme
+    public static void WaitWithThread(int  duration){
+
+        try {
+            Thread.sleep(duration);
+        } catch(InterruptedException e) {
+            System.out.println("got interrupted!");
+        }
+
+    }
+
+    //Timeout ile Bekleme
+    public static void WaitLittleBit(WebDriver driver){
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+
+    }
+
+    //Rastgele sayı üretiyoruz
+    private int GenerateRandomNumber(int size){
+        Random rand = new Random(); 
+        int upperbound = size;
+        int int_random = rand.nextInt(upperbound);
+
+        return int_random;
     }
 
 
